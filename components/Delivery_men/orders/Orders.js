97b824f1823149b22f853686aab22429704b4348
ahmedMohamed <ahmedaltokey320@gmail.com>
2023-10-05 17:21:01 +0300

    5/10

--- components/Delivery_men/orders/Orders.js
+++ components/Delivery_men/orders/Orders.js
@@ -11,16 +11,34 @@ const Orders = () => {
                 <div className={classes.order}>
                     <div className={classes.head}>
                         <p>
-                            طلب رقم 1
+                            طلب رقم : <span>1542</span>
                         </p>
                         <p>
-                            التقيم : <span>5/5</span>
+                            التقيم : <span>5 / -</span>
                         </p>
                         <div className={classes.pending}>
                             <p>جاري التوصيل</p>
                         </div>
                     </div>
                     <div className={classes.info}>
+                        <div className={classes.data}>
+                            <div>
+                                <p>
+                                    اسم المورد : <span>مطعم ماك</span>
+                                </p>
+                                <p>
+                                    رقم المورد : <span>012854758524</span>
+                                </p>
+                            </div>
+                            <div>
+                                <p>
+                                    اسم المستلم : <span>خالد على محمد</span>
+                                </p>
+                                <p>
+                                    رقم المستلم : <span>012895758431</span>
+                                </p>
+                            </div>
+                        </div>
                         <div className={classes.address}>
                             <p>
                                 من : <span>شارع مستشفى حمد السيد </span>
@@ -29,24 +47,132 @@ const Orders = () => {
                                 الى : <span>شارع مستشفى حمد السيد </span>
                             </p>
                         </div>
-                        <div className={classes.number}>
-                            <p>
-                                رقم المستلم منه : <span>012854758524</span>
-                            </p>
-                            <p>
-                                رقم المستلم : <span>012895758431</span>
-                            </p>
-                        </div>
+
                         <div className={classes.moreInfo}>
                             <p>
-                                نوع الطلب : <span> خضار  </span>
+                                نوع الطلب : <span> خضار </span>
                             </p>
                             <p>
                                 يتم التوصيل في : <span>45 دقيقه</span>
                             </p>
                         </div>
-                        <div className={classes.stateOrder}>
+                    </div>
+                    <div className={classes.stateOrder}>
+                        <div className={classes.ready}>
+                            <p>جاهز</p>
+                        </div>
+                        <div className={classes.ready_for_pickup}>
+                            <p>تم الاستلام</p>
+                        </div>
+                        <div className={classes.arrived}>
+                            <p>وصلت</p>
+                        </div>
+                        <div className={classes.completed}>
+                            <p>تم التسليم</p>
+                        </div>
+                        <div className={classes.back}>
+                            <p>عدت</p>
+                        </div>
+                    </div>
+                    <div className={classes.stateOrderTime}>
+                        <div className={classes.ready}>
+                            <p>01:00</p>
+                        </div>
+                        <div className={classes.ready_for_pickup}>
+                            <p>--:--</p>
+                        </div>
+                        <div className={classes.arrived}>
+                            <p>--:--</p>
+                        </div>
+                        <div className={classes.completed}>
+                            <p>--:--</p>
+                        </div>
+                        <div className={classes.back}>
+                            <p>--:--</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={classes.order}>
+                    <div className={classes.head}>
+                        <p>
+                            طلب رقم : <span>1542</span>
+                        </p>
+                        <p>
+                            التقيم : <span>5 / 5</span>
+                        </p>
+                        <div className={classes.complete}>
+                            <p>تم التوصيل</p>
+                        </div>
+                    </div>
+                    <div className={classes.info}>
+                        <div className={classes.data}>
+                            <div>
+                                <p>
+                                    اسم المورد : <span>مطعم ماك</span>
+                                </p>
+                                <p>
+                                    رقم المورد : <span>012854758524</span>
+                                </p>
+                            </div>
+                            <div>
+                                <p>
+                                    اسم المستلم : <span>خالد على محمد</span>
+                                </p>
+                                <p>
+                                    رقم المستلم : <span>012895758431</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div className={classes.address}>
+                            <p>
+                                من : <span>شارع مستشفى حمد السيد </span>
+                            </p>
+                            <p>
+                                الى : <span>شارع مستشفى حمد السيد </span>
+                            </p>
+                        </div>
 
+                        <div className={classes.moreInfo}>
+                            <p>
+                                نوع الطلب : <span> خضار </span>
+                            </p>
+                            <p>
+                                يتم التوصيل في : <span>45 دقيقه</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className={classes.stateOrder}>
+                        <div className={classes.ready}>
+                            <p>جاهز</p>
+                        </div>
+                        <div className={classes.ready_for_pickup}>
+                            <p>تم الاستلام</p>
+                        </div>
+                        <div className={classes.arrived}>
+                            <p>وصلت</p>
+                        </div>
+                        <div className={classes.completed}>
+                            <p>تم التسليم</p>
+                        </div>
+                        <div className={classes.back}>
+                            <p>عدت</p>
+                        </div>
+                    </div>
+                    <div className={classes.stateOrderTime}>
+                        <div className={classes.ready}>
+                            <p>01:00</p>
+                        </div>
+                        <div className={classes.ready_for_pickup}>
+                            <p>01:20</p>
+                        </div>
+                        <div className={classes.arrived}>
+                            <p>01:30</p>
+                        </div>
+                        <div className={classes.completed}>
+                            <p>01:40</p>
+                        </div>
+                        <div className={classes.back}>
+                            <p>01:42</p>
                         </div>
                     </div>
                 </div>
