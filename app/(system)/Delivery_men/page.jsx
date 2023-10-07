8f8f746bd951114ed0a88d7e172fa8138c6fe0dd
ahmedMohamed <ahmedaltokey320@gmail.com>
2023-10-07 15:59:21 +0300

    7/10

--- app/(system)/Delivery_men/page.jsx
+++ app/(system)/Delivery_men/page.jsx
@@ -12,9 +12,17 @@ const page = () => {
                 <div>
                     <select name="" id="">
                         <option value="">فلتر حسب عدد الاوردرات </option>
+                        <option value="">تصاعديا</option>
+                        <option value="">تنازليا</option>
                     </select>
                     <select name="" id="">
                         <option value="">فلتر حسب التقيم</option>
+                        <option value="">1</option>
+                        <option value="">2</option>
+                        <option value="">3</option>
+                        <option value="">4</option>
+                        <option value="">5</option>
+
                     </select>
                 </div>
             </div>
