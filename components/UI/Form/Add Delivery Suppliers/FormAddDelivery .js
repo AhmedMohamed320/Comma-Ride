c6b3b5d801db5e3644957cbe441e9d70ee8389c6
ahmedMohamed <ahmedaltokey320@gmail.com>
2023-10-08 16:51:05 +0300

    ...

--- components/UI/Form/Add Delivery Suppliers/FormAddDelivery .js
+++ components/UI/Form/Add Delivery Suppliers/FormAddDelivery .js
@@ -3,52 +3,79 @@ import classes from "./FormAdd.module.css";
 const FormAddDelivery = () => {
     return (
         <>
-        <form action="" className={classes.form}>
-            <div>
-                <label htmlFor="n1">الاسم</label>
-                <input type="text" placeholder="ادخل اسم الطيار" id="n1" />
-            </div>
+            <form action="" className={classes.form}>
+                <div>
+                    <label htmlFor="n1">الاسم</label>
+                    <input type="text" placeholder="ادخل اسم الطيار" id="n1" />
+                </div>
 
-            <div>
-                <label htmlFor="n2">الرقم القومي</label>
-                <input type="number" placeholder="ادخل الرقم القومي" id="n2" />
-            </div>
+                <div>
+                    <label htmlFor="n2">الرقم القومي</label>
+                    <input
+                        type="number"
+                        placeholder="ادخل الرقم القومي"
+                        id="n2"
+                    />
+                </div>
 
-            <div>
-                <label htmlFor="n3">رقم الهاتف</label>
-                <input type="number" placeholder="ادخل رقم الهاتف " id="n3" />
-            </div>
+                <div>
+                    <label htmlFor="n3">رقم الهاتف</label>
+                    <input
+                        type="number"
+                        placeholder="ادخل رقم الهاتف "
+                        id="n3"
+                    />
+                </div>
 
-            <div>
-                <label htmlFor="n4">العمر</label>
-                <input type="number" placeholder="ادخل عمر الطيار" id="n4" />
-            </div>
+                <div>
+                    <label htmlFor="n4">العمر</label>
+                    <input
+                        type="number"
+                        placeholder="ادخل عمر الطيار"
+                        id="n4"
+                    />
+                </div>
 
-            <div>
-                <label htmlFor="n5">تاريخ بدايه العمل</label>
-                <input type="date" placeholder="ادخل  التاريخ" id="n5" />
-            </div>
+                <div>
+                    <label htmlFor="n5">تاريخ بدايه العمل</label>
+                    <input type="date" placeholder="ادخل  التاريخ" id="n5" />
+                </div>
 
-            <div>
-                <label htmlFor="n6">الشيفت</label>
-                <select id="n6">
-                    <option value="">11:00 - 07:00</option>
-                    <option value="">11:00 - 07:00</option>
-                    <option value="">11:00 - 07:00</option>
-                </select>
-            </div>
-            <div>
-                <label htmlFor="n7">الصوره الشخصيه</label>
-                <input type="file"  id="n7" />
-            </div>
-            <div>
-                <label htmlFor="n8">ملاحظات</label>
-                <textarea type="number" placeholder="اكتب  " id="n8" />
-            </div>
-        </form>
-        <button className={classes.add}>اضافه الطيار</button>
+                <div>
+                    <label htmlFor="n6">الشيفت</label>
+                    <select id="n6">
+                        <option value="">11:00 - 07:00</option>
+                        <option value="">11:00 - 07:00</option>
+                        <option value="">11:00 - 07:00</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="n9">يعمل بنسبه</label>
+                    <input
+                        type="number"
+                        placeholder="ادخل  رقم النسبه "
+                        id="n9"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="n10">يعمل بثابت</label>
+                    <input
+                        type="number"
+                        placeholder="ادخل  الرقم بالجنيه المصري"
+                        id="n10"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="n7">الصوره الشخصيه</label>
+                    <input type="file" id="n7" />
+                </div>
+                <div>
+                    <label htmlFor="n8">ملاحظات</label>
+                    <textarea type="number" placeholder="اكتب  " id="n8" />
+                </div>
+            </form>
+            <button className={classes.add}>اضافه الطيار</button>
         </>
-
     );
 };
 
